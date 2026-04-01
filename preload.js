992dacb88ef2e4da96fa7059393d7868dfaa7119
killogram47-hash <killogram47@gmail.com>
2026-04-01 15:31:47 +0300

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge } = require('electron');
-const packageJson = require('./package.json');
-
-// Expose a limited API to the renderer process
-contextBridge.exposeInMainWorld('catBrowser', {
-    version: packageJson.version || '0.0.0'
-});
